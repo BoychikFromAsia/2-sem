f9aaf7398052a841e2461b722112550ccadc00c6
BoychikFromAsia <igorganchannel@gmail.com>
2025-02-15 17:25:25 +0300

    lab0
    Инициализация проектов.

---
+++ labs_leti_2/tests/tests.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int x, j, k;
+	int** mtx;
+	mtx = new int* [j][k];
+	for (int i = 0;i < j;i++) {
+		for (int i = 0;i < k;i++) {
+			cin >> mtx[j][k];
+		}
+	}
+
+
+	return 0;
+}
