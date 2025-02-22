cdf4a54d85cc3abe517e3b3b86f64f9f4a09017f
BoychikFromAsia <igorganchannel@gmail.com>
2025-02-22 17:51:42 +0300

    исправлены ошибки
    верс 2

--- labs_leti_2/Lab2/Lab2.cpp
+++ labs_leti_2/Lab2/Lab2.cpp
@@ -3,24 +3,93 @@
 
 using namespace std;
 
+template <typename T>
+void insertBeforeNegatives(Queue<T>& queue)
+{
+	int temp_num;
+	for (int i = 0;i < queue.count();i++)
+	{
+		temp_num = queue.unqueue();
+		if (temp_num < 0)
+		{
+			queue.queue(1);
+			i++;
+		}
+		queue.queue(temp_num);
+	}
+}
+
+template <typename T>
+void removeNegatives(Queue<T>& queue)
+{
+	for (int i = 0; i < queue.count();i++)
+	{
+		T temp_num = queue.unqueue();
+		if (temp_num >= 0)
+		{
+			queue.queue(temp_num);
+		}
+		else
+		{
+			i--;
+		}
+	}
+}
+
+template <typename T>
+int countOccurrences(Queue<T>& queue, T counter)
+{
+	int check_num;
+	cout << "Введите число для проверки: ";
+	cin >> check_num;
+	for (int i = 0; i < queue.count();i++)
+	{
+		int temp_num = queue.unqueue();
+		queue.queue(temp_num);
+		if (temp_num == check_num)
+		{
+			counter++;
+		}
+	}
+	if (counter != 0)
+	{
+		cout << "Число "
+			<< check_num
+			<< " встречается "
+			<< counter
+			<< " раз/раза."
+			<< endl;
+	}
+	else
+	{
+		cout << "Данное число не встречается в очереди."
+			<< endl;
+	}
+	return counter;
+}
+
+void displayMenu()
+{
+	cout << "Выберите операцию: \n"
+		<< "1 - Добавление одного элемента \n"
+		<< "2 - Извлечение одного элемента \n"
+		<< "3 - Возвращение количества элементов коллекции \n"
+		<< "4 - Удаление всех элементов коллекции \n"
+		<< "5 - Вставка перед отрицательным числом элемент со значением 1 \n"
+		<< "6 - Удаление всех отрицательных элементов коллекции \n"
+		<< "7 - Подсчет колиества вхождения в очередь значения \n"
+		<< "8 - Закрыть меню \n";
+}
+
 int main()
 {
 	setlocale(LC_ALL, "RU");
 	Queue <int> queue;
 	int choice;
-	int menu = 1;
 	int counter = 0;
-	while (menu != 0)
+	while (true)
 	{
-		cout << "Выберите операцию: \n"
-			<< "1 - Добавление одного элемента \n"
-			<< "2 - Извлечение одного элемента \n"
-			<< "3 - Возвращение количества элементов коллекции \n"
-			<< "4 - Удаление всех элементов коллекции \n"
-			<< "5 - Вставка перед отрицательным числом элемент со значением 1 \n"
-			<< "6 - Удаление всех отрицательных элементов коллекции \n"
-			<< "7 - Подсчет колиества вхождения в очередь значения \n"
-			<< "8 - Закрыть меню \n";
+		displayMenu();
 		cin >> choice;
 		switch (choice)
 		{
@@ -48,66 +117,19 @@ int main()
 			break;
 
 		case 5:  
-			int temp_num;
-			for (int i=0;i < queue.count();i++)
-			{
-				temp_num = queue.unqueue();
-				if (temp_num < 0)
-				{
-					queue.queue(1);
-					i++;
-				}
-				queue.queue(temp_num);
-			}
+			insertBeforeNegatives(queue);
 			break;
 
 		case 6: 
-			for (int i = 0; i < queue.count();i++)
-			{
-				int temp_num = queue.unqueue();
-				if (temp_num >= 0)
-				{
-					queue.queue(temp_num);
-				}
-				else 
-				{
-					i--;
-				}
-			}
+			removeNegatives(queue);
 			break;
 
 		case 7: 
-			int check_num;
-			cout << "Введите число для проверки: ";
-			cin >> check_num;
-			for (int i = 0; i < queue.count();i++)
-			{
-				int temp_num = queue.unqueue();
-				queue.queue(temp_num);
-				if (temp_num == check_num)
-				{
-					counter++;
-				}
-			}
-			if (counter != 0)
-			{
-				cout << "Число "
-					<< check_num
-					<< " встречается "
-					<< counter
-					<< " раз/раза."
-					<< endl;
-			}
-			else
-			{
-				cout << "Данное число не встречается в очереди."
-					<<endl;
-			}
+			countOccurrences(queue, counter);
 			break;
 
 		case 8: 
-			menu = 0;
-			break;
+			return 0;
 
 		default:
 			cout << "Нет такой операции \n";
